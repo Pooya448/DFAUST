5abc8a4abd9712cd42f292af87f63a291bf1ae42
Pooya Kabiri <pooya_kabiri@yahoo.com>
2021-07-27 17:19:30 +0430

    Modify links

--- download.sh
+++ download.sh
@@ -7,26 +7,26 @@ username=$(urle $username)
 password=$(urle $password)
 
 # Download Masks
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=masks.tar.xz' -O 'masks.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=masks.tar.xz' -O 'masks.tar.xz'
 
 # Dowload Registrations
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=registrations_m.hdf5' -O 'registrations_m.hdf5'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=registrations_f.hdf5' -O 'registrations_f.hdf5'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=registrations_m.hdf5' -O 'registrations_m.hdf5'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=registrations_f.hdf5' -O 'registrations_f.hdf5'
 
 # Download Scripts
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=scripts.zip' -O 'scripts.zip'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=scripts.zip' -O 'scripts.zip'
 
 # Download Scan Data
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50002.tar.xz' -O '50002.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50004.tar.xz' -O '50004.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50007.tar.xz' -O '50007.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50009.tar.xz' -O '50009.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50020.tar.xz' -O '50020.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50021.tar.xz' -O '50021.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50022.tar.xz' -O '50022.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50025.tar.xz' -O '50025.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50026.tar.xz' -O '50026.tar.xz'
-wget --post-data "username=$username&password=$password" 'https://download.is.tuebingen.mpg.de/download.php?domain=dfaust&sfile=50027.tar.xz' -O '50027.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50002.tar.xz' -O '50002.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50004.tar.xz' -O '50004.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50007.tar.xz' -O '50007.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50009.tar.xz' -O '50009.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50020.tar.xz' -O '50020.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50021.tar.xz' -O '50021.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50022.tar.xz' -O '50022.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50025.tar.xz' -O '50025.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50026.tar.xz' -O '50026.tar.xz'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=dfaust&sfile=50027.tar.xz' -O '50027.tar.xz'
 
 
 # Uncompressing
